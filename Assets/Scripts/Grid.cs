da0a3c884ac1f67c18ff7de0c755b28a21d23300
Franci <algera.franci@gmail.com>
2021-05-11 12:13:41 +0200

    Grid update

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -7,23 +7,33 @@ public class Grid : MonoBehaviour
 {
     public List<GridLine> grid;
     [SerializeField] private GameObject tilePrefab;
+    public Vector3 drawOffset;
+
 
     private void Start()
     {
+        
         int i = 0;
         int j = 0;
         foreach (GridLine line in grid)
         {
             foreach (Tile tile in line.line)
             {
-                GameObject obj = Instantiate(tilePrefab, new Vector3(j * 1, i * 1, 0), transform.rotation);
-                if(tile.tileType == TileType.Ground)
+                GameObject obj = Instantiate(tilePrefab, transform);
+                obj.transform.localPosition = new Vector3(j * 1, i * 1, 0) - drawOffset;
+                switch (tile.tileType)
                 {
-                    obj.GetComponent<SpriteRenderer>().color = Color.green;
-                }
-                else
-                {
-                    obj.GetComponent<SpriteRenderer>().color = Color.blue;
+                    case TileType.Ground:
+                        obj.GetComponent<SpriteRenderer>().color = Color.green;
+                        break;
+
+                    case TileType.Water:
+                        obj.GetComponent<SpriteRenderer>().color = Color.blue;
+                        break;
+
+                    case TileType.Empty:
+                        obj.GetComponent<SpriteRenderer>().color = Color.clear;
+                        break;
                 }
                 j++;
             }
