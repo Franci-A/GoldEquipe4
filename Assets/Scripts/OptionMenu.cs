bfbd97cbde5e667d37e225558dd0da4ed05d9dcf
Bifflou <gut.enzo02122001@gmail.com>
2021-05-25 14:56:48 +0200

    Menu Option finished

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -6,35 +6,46 @@ using UnityEngine.SceneManagement;
 public class OptionMenu : MonoBehaviour
 {
     public GameObject optionMenuUI;
+    private bool isOpen = false;
+    private bool isMuted;
+    private bool isVibration;
 
     private void Start()
     {
-        optionMenuUI.SetActive(false);
-    }
-
-    public void Resume()
-    {
-        optionMenuUI.SetActive(false);
-        Time.timeScale = 1f;
+        optionMenuUI.SetActive(isOpen);
+        isMuted = PlayerPrefs.GetInt("MUTED") == 1;
+        AudioListener.pause = isMuted;
+        isVibration = PlayerPrefs.GetInt("VIBRATION") == 1;
     }
 
     public void Option()
     {
-        optionMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        isOpen = !isOpen;
+        optionMenuUI.SetActive(isOpen);
     }
 
     public void LoadMenu()
     {
         optionMenuUI.SetActive(false);
-        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 
     public void Retry()
     {
         optionMenuUI.SetActive(false);
-        Time.timeScale = 1f;
         SceneManager.LoadScene("SampleScene");
+        AudioListener.pause = isMuted;
+    }
+    public void Mute()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+        PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
+    }
+    public void Vibration()
+    {
+        isVibration = !isVibration;
+        Debug.Log(isVibration);
+        PlayerPrefs.SetInt("VIBRATION", isVibration ? 1 : 0);
     }
 }
