c28832dd02ffd97bd265504ee29f85711a621b88
Paul <paul.putegnat@edu.devinci.fr>
2021-05-27 11:43:30 +0200

    Auto stash before merge of "Prog" and "origin/Prog"

---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    [SerializeField] private GameObject snapImage;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
