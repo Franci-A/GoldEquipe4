440a987335d7dffd6edb754911e6980bbe89b71c
Franci <algera.franci@gmail.com>
2021-05-31 11:48:52 +0200

    Gameover

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
-    
+    [System.Serializable]
+    struct Parallax
+    {
+        public List<GameObject> gameObjectsInParallax;
+        public float speed;
+        public Vector3 StartPos;
+        public Vector3 EndPos;
+    }
+
+    [SerializeField] List<Parallax> parallaxes;
+
+    private void Update()
+    {
+        foreach (Parallax item in parallaxes)
+        {
+            foreach (GameObject obj in item.gameObjectsInParallax)
+            {
+                obj.transform.Translate(item.EndPos * item.speed * Time.deltaTime);
+                if (obj.transform.position.x >= item.EndPos.x -.2f && obj.transform.position.x <= item.EndPos.x + .2f)
+                {
+                    obj.transform.position = item.StartPos;
+                }
+            }
+        }
+    }
 }
